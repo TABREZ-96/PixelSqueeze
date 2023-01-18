4f366908e556c16ee7886de244b8b493cb6c4e44
TABREZ SAYED <114156392+TABREZ-96@users.noreply.github.com>
2023-01-18 12:21:25 +0530

    Add files via upload

---
+++ PixSqueze.py
@@ -0,0 +1,12 @@
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+
+file_path = askopenfilename()
+img = PIL.Image.open(file_path)
+myHeight,mywidth = img.size
+
+img = img.resize((myHeight,mywidth), PIL.Image.ANTIALIAS)
+save_path = askopenfilename()
+
+img.save(save_path+"_compressed.jpg")
